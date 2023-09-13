447240504fc65a06e9c942d1bd1646b7f6495087
Flopiras <Flopiras@yahoo.it>
2023-09-13 16:56:22 +0200

    add hardcoded mail

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ContactMessageMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function sendMessage()
     {
-        return response()->json('prova');
+        $mail =  new ContactMessageMail(
+            sender: 'pippo@pippo.oi',
+            object: 'test',
+            content: 'blablabla'
+        );
+        Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
+
+        return response(204);
     }
 }
