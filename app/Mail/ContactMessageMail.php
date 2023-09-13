11d2d75dc99e518405512bae5dce2748a7572b44
Flopiras <Flopiras@yahoo.it>
2023-09-13 16:56:36 +0200

    edit methods

--- app/Mail/ContactMessageMail.php
+++ app/Mail/ContactMessageMail.php
@@ -33,7 +33,8 @@ class ContactMessageMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Contact Message Mail',
+            from: $this->sender,
+            subject: $this->object,
         );
     }
 
@@ -43,7 +44,8 @@ class ContactMessageMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'mails.contacts.message',
+            with: ['content' => $this->content]
         );
     }
 
